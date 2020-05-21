d406b7083cb442664c43fb4a0bfa2c1a84013519
David Paiva <pillgp@icloud.com>
2020-05-21 12:05:32 +0100

    Primeiro commit!

---
+++ PilhaDeTrabalhos/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Input = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 105)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(119, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Adicionar Trabalho"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(12, 134)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(119, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Remover Trabalho"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 163)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(119, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Contar Trabalhos"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(12, 192)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(119, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "Verificar Trabalho"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(12, 221)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(119, 23)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Limpar Pilha"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Input
+        '
+        Me.Input.Location = New System.Drawing.Point(12, 12)
+        Me.Input.Name = "Input"
+        Me.Input.Size = New System.Drawing.Size(776, 20)
+        Me.Input.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(137, 56)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(651, 272)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 340)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Input)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Pilha de Trabalhos"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Input As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ PilhaDeTrabalhos/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Dim Trabalhos As New Stack
+
+    Sub UpdateData()
+        Input.Clear()
+        TextBox2.Clear()
+
+        For i = 0 To Trabalhos.Count - 1
+            TextBox2.Text &= Trabalhos(i) & vbNewLine
+        Next
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Trabalhos.Push(Input.Text)
+        UpdateData()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Trabalhos.Pop()
+        UpdateData()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("A pilha tem " & Trabalhos.Count & " trabalhos.")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Trabalhos.Contains(Input.Text) Then
+            MessageBox.Show("O trabalho " & Input.Text & " existe.")
+        Else
+            MessageBox.Show("O trabalho " & Input.Text & " não existe.")
+        End If
+
+        Input.Clear()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Trabalhos.Clear()
+        UpdateData()
+    End Sub
+End Class
